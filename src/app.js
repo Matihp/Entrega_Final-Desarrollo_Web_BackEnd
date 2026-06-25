265613acbec77852477a44cbbed4a05b46af83d3
Maria Lopez <lopezmariasoledad2020@gmail.com>
2026-06-25 01:23:04 -0300

    add user info and logout button to navbar

--- src/app.js
+++ src/app.js
@@ -50,6 +50,11 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
 
+app.use((req, res, next) => {
+    res.locals.user = req.user;
+    next();
+});
+
 app.use(express.static(path.join(__dirname, '../public')));
 
 // Middleware de auditoria
